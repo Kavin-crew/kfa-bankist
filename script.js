2b951823749784e19ed8093ae8eade9239019a4a
Kavin Abregana <abreganakavin@gmail.com>
2023-07-06 00:15:56 +0800

    working with bubbling concept

--- script.js
+++ script.js
@@ -158,3 +158,47 @@ document.addEventListener('keydown', function (e) {
 //   // to scroll to the desired section (new way)
 //   section1.scrollIntoView({ behavior: 'smooth' });
 // });
+
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function (e) {
+//   alert('Alert message');
+
+//   // to remove the event
+//   h1.removeEventListener('mouseenter', alertH1);
+// };
+
+// h1.addEventListener('mouseenter', alertH1);
+
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1));
+
+// // old ways of adding event
+// h1.onmouseenter = function () {
+//   alert('onmouseenter alert');
+// };
+
+// create random colors
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
